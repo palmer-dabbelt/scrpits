93754b388fdeae39b7a2627d2a96438c2dc3b507
Palmer Dabbelt <palmer@dabbelt.com>
2011-05-26 23:37:36 -0400

    updates, not really shure what

--- src/vplayer.bash
+++ src/vplayer.bash
@@ -1,4 +1,4 @@
-mplayer="mplayer -volume 50 -cache 100000 -cache-min 1 -cache-seek-min 1"
+mplayer="mplayer -cache 100000 -cache-min 1 -cache-seek-min 1"
 addons=""
 
 codec=`midentify "$1" | grep ID_VIDEO_CODEC | tail -1 | cut -d '=' -f '2'`
