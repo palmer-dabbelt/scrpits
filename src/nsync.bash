498b9ffb3c35ca701af94ab4263b1f46aa56e0dc
Palmer Dabbelt <palmer@dabbelt.com>
2011-06-11 14:17:08 -0400

    added desktop.lulu to nsync

--- src/nsync.bash
+++ src/nsync.bash
@@ -8,3 +8,11 @@ then
 	
 	ionice -n 7 -c 2 $binary desktop-palmer-dabbelt-com
 fi
+
+if [[ "$local" == "desktop.lulu.dabbelt.com" ]]
+then
+	echo "desktop.lulu.dabbelt.com <==> server.dabbelt.com"
+	
+	ionice -n 7 -c 2 $binary desktop-lulu-dabbelt-com
+fi
+
