820c2588305b7475a98d278f62596142da6a276a
Palmer Dabbelt <palmer@dabbelt.com>
2011-10-22 14:18:25 -0400

    chkindent properly check for files

--- src/chkindent.bash
+++ src/chkindent.bash
@@ -3,40 +3,43 @@ fix="$2"
 
 if [[ "$dir" == "--fix" ]]
 then
-	dir=""
-	fix="--fix"
+    dir=""
+    fix="--fix"
 fi
 
 if [[ "$dir" == "" ]]
 then
-	dir="."
+    dir="."
 fi
 
 if test -f "$dir"
 then
-	if [[ "$fix" == "--fix" ]]
-	then
-		indent "$dir"	
-	else
-		cat "$dir" | indent | diff - $dir
-	fi
-
-	exit $?
+    if [[ "$fix" == "--fix" ]]
+    then
+	indent "$dir"	
+    else
+	cat "$dir" | indent | diff - $dir
+    fi
+    
+    exit $?
 fi
 
 return="0"
 for f in `find "$dir" -name "*.[ch]"`
 do
+    if [[ `stat "$f" --format %F` == "regular file" ]]
+    then
 	cat "$f" | indent | diff - $f > /dev/null
 	if [[ "$?" != "0" ]]
 	then
-		return="1"
-		echo $f
-		if [[ "$fix" == "--fix" ]]
-		then
-			indent "$f"
-		fi
+	    return="1"
+	    echo $f
+	    if [[ "$fix" == "--fix" ]]
+	    then
+		indent "$f"
+	    fi
 	fi
+    fi
 done
 
 exit $return
