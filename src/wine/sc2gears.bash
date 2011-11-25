e8b835add6cbef48659a9184339226b6fb7dbcf7
Palmer Dabbelt <palmer.dabbelt@gmail.com>
2011-11-25 16:16:48 -0500

    added sc2gears

---
+++ src/wine/sc2gears.bash
@@ -0,0 +1,5 @@
+export WINEDBG=-all
+export WINEPREFIX=$HOME/.wineprefix/wingsoliberty/
+
+cd $WINEPREFIX/drive_c/users/palmer/Sc2gears/
+sh Sc2gears-linux.sh
--- src/wine/wingsoliberty.bash
+++ src/wine/wingsoliberty.bash
@@ -1,5 +1,5 @@
 export WINEDBG=-all
 export WINEPREFIX=$HOME/.wineprefix/wingsoliberty/
 
-cd $HOME/.wineprefix/wingsoliberty/drive_c/Program\ Files/StarCraft\ II/
+cd $WINEPREFIX/drive_c/Program\ Files/StarCraft\ II/
 wine "StarCraft II.exe"
