fc1184282cd1670ca519c1bd203bd3943a221463
Palmer Dabbelt <palmer.dabbelt@gmail.com>
2011-11-25 19:10:03 -0500

    added broodwar, whoops :)

---
+++ src/wine/broodwar.bash
@@ -0,0 +1,8 @@
+export WINEDBG=-all
+export WINEPREFIX=$HOME/.wineprefix/broodwar/
+
+#cd $WINEPREFIX/drive_c/Program\ Files/Starcraft/
+#wine "StarCraft.exe"
+
+cd $WINEPREFIX/drive_c/Program\ Files/ICCup/Launcher/
+wine "Launcher.exe"
