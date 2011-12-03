afed52ad2c9dc424f032ff127ae052a7a6fe8be3
Palmer Dabbelt <palmer.dabbelt@gmail.com>
2011-12-03 17:48:24 -0500

    quiet starcraft 2

--- src/wine/wingsoliberty.bash
+++ src/wine/wingsoliberty.bash
@@ -2,4 +2,4 @@ export WINEDBG=-all
 export WINEPREFIX=$HOME/.wineprefix/wingsoliberty/
 
 cd $WINEPREFIX/drive_c/Program\ Files/StarCraft\ II/
-wine "StarCraft II.exe"
+wine "StarCraft II.exe" >& /dev/null
