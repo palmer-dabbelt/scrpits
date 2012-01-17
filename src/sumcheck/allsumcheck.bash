6fa77fb0875aaa37b6b8d6a24c185f387d5c54d2
Palmer Dabbelt <palmer.dabbelt@gmail.com>
2012-01-16 23:09:55 -0500

    Added some checksumming code

---
+++ src/sumcheck/allsumcheck.bash
@@ -0,0 +1,14 @@
+origdir=`pwd`
+retval=0
+
+find . -type d -print0 | while read -d $'\0' dir
+do
+    echo "Runing sumcheck in \"$dir\""
+    cd "$dir"
+    sumcheck
+    if [[ "$?" != 0 ]]
+    then
+	retval=1
+    fi
+    cd "$origdir"
+done
---
+++ src/sumcheck/sumcheck.bash
@@ -0,0 +1,24 @@
+of=".sha256sums"
+
+# Makes sure the file exists
+touch "$of"
+
+# Adds everything that hasn't yet been added to the directory listing
+find . -maxdepth 1 -type f ! -iname "$of" -print0 | while read -d $'\0' file
+do
+    echo $file
+    out=$(cat "$of" | cut -c 67- | grep "^$file$")
+    if [[ "$out" != "$file" ]]
+    then
+	shortfile=$(echo "$file" | cut -c 3-)
+	echo "Updating sum for \"$shortfile\""
+	sha256sum "$file" >> "$of"
+    fi
+done
+
+# Actually preforms the verification
+files=$(cat .sha256sums | wc -l)
+if [[ "$files" != 0 ]]
+then
+    sha256sum -c "$of"
+fi
