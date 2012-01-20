63d08017400df492c9f952e26e290451b53260d0
Palmer Dabbelt <palmer.dabbelt@gmail.com>
2012-01-20 18:47:45 -0500

    Fixed mailaddr

--- src/mailaddr/main.bash
+++ src/mailaddr/main.bash
@@ -1,3 +1,3 @@
-file=$(grep -i "fn:" ~/.pim/contacts/* | grep "lulu" | cut -d ':' -f 1)
+file=$(grep -i "fn:" ~/.pim/contacts/* | grep "$1" | cut -d ':' -f 1)
 grep "EMAIL:" "$file" | cut -d ':' -f 2
 
