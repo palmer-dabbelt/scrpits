695633358f0e6c93ee7adbf81ecb8e9d3510fb09
Palmer Dabbelt <palmer.dabbelt@gmail.com>
2012-01-29 22:24:47 -0500

    Ran indent

--- src/v4l2/v4l2ppm.c
+++ src/v4l2/v4l2ppm.c
@@ -1,3 +1,4 @@
+
 /* part of media-libs/libv4l */
 #include <libv4l2.h>
 
@@ -20,31 +21,31 @@ int main(int argc, char **argv)
     int err;
     int video_dev;
     static struct v4l2_format format;
-    uint8_t * image_data;
+    uint8_t *image_data;
     int image_width, image_height;
     char *output_filename;
-    
+
     /* Opens the video VIDEO_PATH using the wrapped call */
     video_dev = v4l2_open(VIDEO_PATH, O_RDWR);
     assert(video_dev > 0);
-    
+
     /* Zeros out the format, just in case */
     memset(&format, 0x00, sizeof(format));
-    
+
     /* Fills in the format type to get BGR24 */
     format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     format.fmt.pix.pixelformat = V4L_FORMAT;
     format.fmt.pix.width = IMAGE_WIDTH;
     format.fmt.pix.height = IMAGE_HEIGHT;
-    
+
     /* Calls the IOCTL to set the format */
     err = v4l2_ioctl(video_dev, VIDIOC_S_FMT, &format);
     assert(err == 0);
-    
+
     /* The call _will_ succeed because we are using the wrapper, but to make
-       sure we can test anyway */
+     * sure we can test anyway */
     assert(format.fmt.pix.pixelformat == V4L_FORMAT);
-    
+
     /* Creates some space to store the data */
     image_width = format.fmt.pix.width;
     image_height = format.fmt.pix.height;
@@ -53,23 +54,23 @@ int main(int argc, char **argv)
 
     /* Figures out where to save the photo */
     if (argc == 1)
-	output_filename = IMAGE_PATH;
+        output_filename = IMAGE_PATH;
     else if (argc == 2)
-	output_filename = argv[1];
+        output_filename = argv[1];
     else
-	abort();
-    
+        abort();
+
     /* Actually takes the picture */
     {
-	int size;
-	FILE * file;
-	
-	size = 3 * image_width * image_height;
-	v4l2_read(video_dev, image_data, size);
-	
-	file = fopen(output_filename, "w");
-	fprintf(file, "P6\n%d %d\n%d\n", image_width, image_height, 255);
-	fwrite(image_data, size, 1, file);
-	fclose(file);
+        int size;
+        FILE *file;
+
+        size = 3 * image_width * image_height;
+        v4l2_read(video_dev, image_data, size);
+
+        file = fopen(output_filename, "w");
+        fprintf(file, "P6\n%d %d\n%d\n", image_width, image_height, 255);
+        fwrite(image_data, size, 1, file);
+        fclose(file);
     }
 }
