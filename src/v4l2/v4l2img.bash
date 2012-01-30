0aeb8ae4c928d1d2dddd84bef2a352b24d2286da
Palmer Dabbelt <palmer.dabbelt@gmail.com>
2012-01-29 22:20:19 -0500

    Added some v4l2 scripts
    v4l2img: Creates an image (any kind "convert" supports) from a camera.
    
    v4l2ppm: Captures an image from a camera and dumps it out as PPM.

---
+++ src/v4l2/v4l2img.bash
@@ -0,0 +1,14 @@
+set -x
+
+echo "$1"
+
+if [[ "$1" == "" ]]
+then
+    echo "$0 <filename>"
+    exit 1
+fi
+
+temp=`mktemp -d`
+v4l2ppm "$temp"/v4l2.ppm
+convert "$temp"/v4l2.ppm "$1"
+rm -rf "$temp"
