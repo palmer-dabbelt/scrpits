83d7c7180bcf50dad1bf5731e69b8693240289bc
Palmer Dabbelt <palmer@desktop.palmer.dabbelt.com>
2012-02-09 23:05:15 -0500

    Wrong prefix

--- src/wine/wingsoliberty.bash
+++ src/wine/wingsoliberty.bash
@@ -1,5 +1,5 @@
 export WINEDBG=-all
-export WINEPREFIX="$HOME/.local/share/wineprefix"
+export WINEPREFIX="$HOME/.local/share/wineprefix/wingsoliberty"
 
 cd "$HOME/.local/share/wineprefix/wingsoliberty/drive_c/Program Files (x86)/StarCraft II"
 wine "StarCraft II.exe" >& /dev/null
