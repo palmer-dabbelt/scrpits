1da52e8063da65f6dbfa916eaf56e7a320763b0a
Palmer Dabbelt <palmer@desktop.palmer.dabbelt.com>
2012-02-12 19:01:59 -0500

    Force SC2's affinity

--- src/wine/wingsoliberty.bash
+++ src/wine/wingsoliberty.bash
@@ -2,4 +2,4 @@ export WINEDBG=-all
 export WINEPREFIX="$HOME/.local/share/wineprefix/wingsoliberty"
 
 cd "$HOME/.local/share/wineprefix/wingsoliberty/drive_c/Program Files (x86)/StarCraft II"
-wine "StarCraft II.exe" >& /dev/null
+schedtool -a 1 -e wine "StarCraft II.exe" >& /dev/null
