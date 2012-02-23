74b798d1ae2f143194b470e0d5ad22b0a1dfac6a
Palmer Dabbelt <palmer@desktop.palmer.dabbelt.com>
2012-02-22 19:41:19 -0500

    Moved wings patha gain

--- src/wine/wingsoliberty.bash
+++ src/wine/wingsoliberty.bash
@@ -1,5 +1,5 @@
 export WINEDBG=-all
 export WINEPREFIX="$HOME/.local/share/wineprefix/wingsoliberty"
 
-cd "$HOME/.local/share/wineprefix/wingsoliberty/drive_c/Program Files (x86)/StarCraft II"
+cd "$HOME/.local/share/wineprefix/wingsoliberty/drive_c/Program Files/StarCraft II"
 schedtool -a 1 -e wine "StarCraft II.exe" >& /dev/null
