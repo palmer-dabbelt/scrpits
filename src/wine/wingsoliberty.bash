fbcbb82fde8a5c03b5f9fa05d09f6014a45f8611
Palmer Dabbelt <palmer@desktop.palmer.dabbelt.com>
2012-02-25 15:22:45 -0500

    Better CPU masking for sc2

--- src/wine/wingsoliberty.bash
+++ src/wine/wingsoliberty.bash
@@ -2,4 +2,4 @@ export WINEDBG=-all
 export WINEPREFIX="$HOME/.local/share/wineprefix/wingsoliberty"
 
 cd "$HOME/.local/share/wineprefix/wingsoliberty/drive_c/Program Files/StarCraft II"
-schedtool -a 1 -e wine "StarCraft II.exe" >& /dev/null
+schedtool -a 0x1 -e wine "StarCraft II.exe" >& /dev/null
