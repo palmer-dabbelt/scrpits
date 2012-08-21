8cbaf88110a271c69119b6fa5d0a07a68c7587e8
Palmer Dabbelt <palmer@dabbelt.com>
2012-08-21 16:59:08 -0700

    Updates getmail
    I no longer use "inc" because I don't want NMH polluting my system, so
    I'm now just using fetchmail and mhng-pipe-*.

--- src/getmail.bash
+++ src/getmail.bash
@@ -1,3 +1,9 @@
-# Grabs any new mail
-fetchmail -s
-inc
+last_message=$(scan inbox | tail -1 | xargs echo | cut -d ' ' -f 1)
+
+fetchmail || [ $? -eq 1 ]
+if [[ "$?" == "0" ]]
+then
+    show $(($last_message + 1)) >& /dev/null
+    scan
+fi
+
