a1d809d267cc376fe1d134f6e729724737fc3733
Palmer Dabbelt <palmer@dabbelt.com>
2012-10-12 16:44:27 -0700

    Parallelize getmail

--- src/getmail.bash
+++ src/getmail.bash
@@ -1,6 +1,11 @@
 orig=$(mhng-pipe-folder_info --last-message +inbox)
 
-fetchmail
+cat .fetchmailrc \
+    | grep -i ^poll \
+    | cut -d ' ' -f 2 \
+    | sed 's/.*/fetchmail-\0-pid \0/' \
+    | parallel -C ' ' fetchmail --pidfile
+
 if [[ "$?" == "0" ]]
 then
     fin=$(mhng-pipe-folder_info --last-message +inbox)
