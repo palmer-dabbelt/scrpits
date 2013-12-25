611180fc62c0a2dec67aee77e55d2584f799a102
Palmer Dabbelt <palmer@dabbelt.com>
2013-12-25 01:02:24 -0800

    Add a bunch of junk to pacupdate

--- src/pacupdate.bash
+++ src/pacupdate.bash
@@ -1,10 +1,22 @@
 set -ex
 
+# Ensure that my configuration is updated
 cd /etc
 git pull
-cd
+cd ~
+git pull
 
+# Here's where I actually update everything (including build-time
+# dependencies, which is recessary for revdep-rebuild)
 emerge -avNDu --with-bdeps=y @world "$@"
+
+# Scan through my system to ensure that everything is in working order
+emerge -av @preserved-rebuild
 revdep-rebuild
+perl-cleaner --modules
+python-updater
+haskell-updater
+
+# Remove anything unnecessary that was installed during the update
+# process
 emerge -av --depclean
-revdep-rebuild
