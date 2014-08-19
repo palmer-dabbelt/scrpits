44b59870466df62d32271c12fa0db247aaa7513f
Palmer Dabbelt <palmer@dabbelt.com>
2014-08-19 15:02:38 -0700

    Check the rest of the updater programs too
    This way if I don't install a bunch of little things then my code
    won't fall apart.

--- src/pacupdate.bash
+++ src/pacupdate.bash
@@ -13,13 +13,26 @@ emerge -avNDu --with-bdeps=y @world "$@"
 # Scan through my system to ensure that everything is in working order
 emerge -av @preserved-rebuild
 revdep-rebuild
-perl-cleaner --modules
-python-updater
+
+if [[ `which perl-cleaner 2>/dev/null` != "" ]]
+then
+    perl-cleaner --modules
+fi
+
+if [[ `which python-updater 2>/dev/null` != "" ]]
+then
+    python-updater
+fi
+
 if [[ `which haskell-updater 2>/dev/null` != "" ]]
 then
     haskell-updater
 fi
-emacs-updater
+
+if [[ `which emacs-updater 2>/dev/null` != "" ]]
+then
+    emacs-updater
+fi
 
 # Remove anything unnecessary that was installed during the update
 # process
